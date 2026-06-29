a4a89009dab932b482bd3a992cd3df9762f9bd8a
Mirjalol Mirxomitov <miramirxomitov97@gmail.com>
2026-06-29 17:26:10 +0500

    feat(layout): wire AppHeader and AppFooter into default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="min-h-screen bg-gray-50">
-    <slot />
+  <div class="min-h-screen flex flex-col bg-gray-50">
+    <LayoutAppHeader />
+    <main class="flex-1">
+      <slot />
+    </main>
+    <LayoutAppFooter />
   </div>
 </template>
